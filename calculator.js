2561344c99e4fdf52b780522403d91130a5b89d2
waxingrose <phnx7rch@gmail.com>
2025-05-07 11:33:11 -0700

    change apostrophes

--- calculator.js
+++ calculator.js
@@ -1,30 +1,31 @@
-const view = document.querySelector(".view")
-const numbers = document.querySelectorAll(".number")
-const operators = document.querySelectorAll(".operator")
-const equal = document.querySelector(".equal")
-const clear = document.querySelector(".clear")
-const sign = document.querySelector(".sign")
-const percent = document.querySelector(".percent")
+const view = document.querySelector('.view')
+const numbers = document.querySelectorAll('.number')
+const operators = document.querySelectorAll('.operator')
+const equal = document.querySelector('.equal')
+const clear = document.querySelector('.clear')
+const sign = document.querySelector('.sign')
+const percent = document.querySelector('.percent')
 
-let digit = ""
+let digit = ''
 let storedDigit = []
 numbers.forEach((num) =>
-  num.addEventListener("click", () => {
+  num.addEventListener('click', () => {
     // if the result is displayed, a new number clears and starts new calc
     if (result && storedDigit.lastIndexOf(op) == -1) { // bug with non single digit; reset digit at equal instead of here
-      result = ""
+      result = ''
       storedDigit = []
     }
     if (result == Infinity || result == -Infinity) {
-      result = ""
-      digit = ""
+      result = ''
+      digit = ''
     }
     // insert code here to disable more than one decimal 
 
     digit += num.innerHTML
     view.innerHTML = digit
 
-    console.log("digit = " + digit)
+    console.log('digit = ' + digit)
+
   }),
 )
 
@@ -37,7 +38,7 @@ function operate() {
     }
 
     if (accum.length === 3) {
-      formula = accum.join("")
+      formula = accum.join('')
       let newAccum = eval(formula)
       accum = [newAccum]
     }
@@ -45,17 +46,17 @@ function operate() {
     return accum.concat(current) // doesn't iterate last index of new array if using splice here
   }, [])
 
-  result = eval(accum.join(""))
+  result = eval(accum.join(''))
   console.log(accum)
 }
 
 let op
 operators.forEach((operator) =>
-  operator.addEventListener("click", () => {
+  operator.addEventListener('click', () => {
 
     // fixes [2,0,0] and [2,20,200]
-    if (digit.length != 0) { 
-      storedDigit.push(digit) 
+    if (digit.length != 0) {
+      storedDigit.push(digit)
     }
 
     // only show result with enough input
@@ -68,19 +69,19 @@ operators.forEach((operator) =>
 
     op = operator.innerHTML
     storedDigit.push(op)
-    digit = "" // resets digit 
+    digit = '' // resets digit 
 
-    console.log("storedDigit = " + storedDigit)
+    console.log('storedDigit = ' + storedDigit)
     console.log(digit)
 
   }),
 )
 
-equal.addEventListener("click", (event) => {
+equal.addEventListener('click', (event) => {
 
   // fixes [2,0,0] and [2,20,200]
   if (digit.length != 0) {
-    storedDigit.push(digit) 
+    storedDigit.push(digit)
   }
   operate()
   // clear storedDigit and push result into storedDigit for when pressing operator after equal
@@ -90,49 +91,42 @@ equal.addEventListener("click", (event) => {
     view.innerHTML = result.toPrecision(20)
   }
   if (result === Infinity || result === -Infinity) {
-    view.innerHTML = "=("
+    view.innerHTML = '=('
   } else {
     view.innerHTML = result
   }
   digit = '' // reset for new number
-  console.log("result = " + result)
-  console.log("storedDigit = " + storedDigit)
+  console.log('result = ' + result)
+  console.log('storedDigit = ' + storedDigit)
 })
 
 function neg(number) {
   return -number
 }
-sign.addEventListener("click", (event) => {
-  if (view.innerHTML == digit) {
+sign.addEventListener('click', (event) => {
+  if (storedDigit.length != 1 && !result) {
     digit = neg(digit)
     view.innerHTML = digit
   }
+  
 })
 
 function per(number) {
   return number / 100
 }
-percent.addEventListener("click", (event) => {
-  if (view.innerHTML == digit) {
-    digit = per(digit)
+percent.addEventListener('click', (event) => {
+  if (Number(view.innerHTML)) {
+    digit = per(view.innerHTML)
     view.innerHTML = digit
-  }
-  if (view.innerHTML == storedDigit) {
-    storedDigit = per(storedDigit)
-    view.innerHTML = storedDigit
-  }
-  if (view.innerHTML == result) {
-    result = per(result)
-    view.innerHTML = result
   }
 })
 
 const resetView = view.innerHTML
-clear.addEventListener("click", (event) => {
-  result = ""
+clear.addEventListener('click', (event) => {
+  result = ''
   storedDigit = []
-  digit = ""
-  op = ""
-  formula = ""
+  digit = ''
+  op = ''
+  formula = ''
   view.innerHTML = resetView
 })
